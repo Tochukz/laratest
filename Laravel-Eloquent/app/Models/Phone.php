c3d2de31c2eee61fd5844f6a38d0cf20710c2257
Tochukwu Nwachukwu <truetochukz@gmail.com>
2021-02-18 22:38:30 +0200

    Laravel Eloquent

--- Laravel-Eloquent/app/Models/Phone.php
+++ Laravel-Eloquent/app/Models/Phone.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Phone extends Model
 {
     public function user(){
-        //return $this->belongTo('App]\Models\User', 'foreign_key', 'local_key');
+        //return $this->belongTo('App\Models\User', 'foreign_key', 'local_key');
         /* Fllowing convention where foreign key is user_id */
         return $this->belongsTo('App\Models\User'); 
     }
